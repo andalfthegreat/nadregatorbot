a48dc12d985bdd9f12c9a5ffd42ace4bcc524479
andalfthegreat <132973120+andalfthegreat@users.noreply.github.com>
2025-06-27 19:35:00 +0200

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,21 +1,21 @@
 import os
 from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import declarative_base
 from sqlalchemy import inspect
 
 Base = declarative_base()
 
-# Define your model(s)
 class User(Base):
     __tablename__ = 'users'
-
     id = Column(Integer, primary_key=True)
     telegram_id = Column(Integer, unique=True)
     name = Column(String)
 
-
 def create_tables():
     database_url = os.environ.get("DATABASE_URL")
+    if not database_url:
+        raise ValueError("DATABASE_URL is not set. Check your environment variables.")
+
     engine = create_engine(database_url)
     inspector = inspect(engine)
 
